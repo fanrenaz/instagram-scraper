77706306fbcb7357b44e03c003beb66cbd34f548
Yifan Ren <51114968+fanrenaz@users.noreply.github.com>
2020-02-20 23:30:42 -0500

    Update cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -26,3 +26,18 @@ def tag_join(l):
         l = ",".join(l)
     except: pass
     return l
+
+def json_to_df(file):
+    with open(file, 'r') as f:
+        js = json.load(f)
+    df = pd.DataFrame(js["GraphImages"])
+    df["dimensions"] = df["dimensions"].map(lambda line: str(line.get("height"))+"*"+str(line.get("width")))
+    df["edge_liked_by"] = df["edge_liked_by"].map(lambda line: line.get("count"))
+    df["edge_media_preview_like"] = df["edge_media_preview_like"].map(lambda line: line.get("count"))
+    df["edge_media_to_caption"] = df["edge_media_to_caption"].map(extract_node_text)
+    df["edge_media_to_comment"] = df["edge_media_to_comment"].map(lambda line: line.get("count"))
+    df["owner"] = df["owner"].map(lambda line: line.get("id"))
+    df["tags"] = df["tags"].map(tag_join)
+    df["comments_disabled"] = df["comments_disabled"].map(lambda x: None if math.isnan(x) is True else x)
+    df["video_view_count"] = df["video_view_count"].map(lambda x: x if type(x)==float else None)
+    return df
